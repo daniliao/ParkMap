4ff6d6ccfb79138aec518c3bcae031ff7e2a6da8
Daniel Liao <danielliao0426@gmail.com>
2025-03-17 21:31:31 -0700

    Initial Commit

---
+++ favoriteParkTests/favoriteParkTests.swift
@@ -0,0 +1,17 @@
+//
+//  favoriteParkTests.swift
+//  favoriteParkTests
+//
+//  Created by Daniel on 3/17/25.
+//
+
+import Testing
+@testable import favoritePark
+
+struct favoriteParkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
